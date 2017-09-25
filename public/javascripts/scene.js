d378e6356417e186d2c87c4a1c5b23c34fdb25ba
Kurt Milon <with4@outlook.com>
2017-09-25 18:29:51 +0900

    commit

--- public/javascripts/scene.js
+++ public/javascripts/scene.js
@@ -22,6 +22,18 @@ var isMouseDown = false;
 var keyboard = new THREEx.KeyboardState();
 // var clock = new THREE.Clock();
 
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+var prevTime = performance.now();
+var velocity = new THREE.Vector3();
+var direction = new THREE.Vector3();
+
+var raycaster;
+
 init();
 animate();
 
@@ -34,34 +46,6 @@ function init()
     // Append the canvas element created by the renderer to document body element.
     container = document.createElement('div');
     document.body.appendChild(container);
-
-    /***********************/
-    /*       SCENE        */
-    /***********************/
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x505050 );
-    
-    /***********************/
-    /*       CAMERA        */
-    /***********************/
-    camera = new THREE.PerspectiveCamera(
-        75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    scene.add(camera);
-    camera.position.set(0, 1, 1);
-
-    /***********************/
-    /*     CROSS-HAIR      */
-    /***********************/
-    crosshair = new THREE.Mesh(
-        new THREE.RingGeometry(0.02, 0.04, 32),
-        new THREE.MeshBasicMaterial({
-            color: 0xffffff,
-            opacity: 0.3,
-            transparent: true
-        })
-    );
-    crosshair.position.z = -2;
-    camera.add(crosshair);
 
     /***********************/
     /*       RENDERER      */
@@ -79,13 +63,80 @@ function init()
     /***********************/
     /*       WEBVR         */
     /***********************/
-    renderer.vr.enabled = true;
     WEBVR.getVRDisplay(function (display) {
-        //camera.position.set(24.2456, 1.6911, 15.984);
+        renderer.vr.enabled = true;
         renderer.vr.setDevice(display);
-        camera.position.set(24.2456, 1.6911, 15.984);
+        //camera.position.set(24.2456, 1.6911, 15.984);
         document.body.appendChild(WEBVR.getButton(display, renderer.domElement));
     });
+
+    /***********************/
+    /*       SCENE        */
+    /***********************/
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
+    scene.fog = new THREE.Fog(0xffffff, 0, 750);
+
+    /***********************/
+    /*      LIGHT          */
+    /***********************/
+    var light = new THREE.HemisphereLight( 0xfff0f0, 0x606066 );
+    // var light = new THREE.AmbientLight(0xFFFFFF);
+    scene.add(light);
+
+    /***********************/
+    /*       CAMERA        */
+    /***********************/
+    camera = new THREE.PerspectiveCamera(
+        75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    scene.add(camera);
+    camera.position.set(1, 0, 1);
+
+    /***********************/
+    /*      CONTROLS       */
+    /***********************/
+    // controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls = new THREE.PointerLockControls(camera);
+    scene.add(controls.getObject());
+
+    // pointerLock
+    var havePointerLock = 
+        'pointerLockElement' in document ||
+        'mozPointerLockElement' in document ||
+        'webkitPointerLockElement' in document;
+    if (havePointerLock) {
+        var element = document.body;
+        var pointerlockchange = function (event) {
+            if (document.pointerLockElement === element ||
+                document.mozPointerLockElement === element ||
+                document.webkitPointerLockElement == element) {
+                
+                controlsEnabled = true;
+                controls.enabled = true;
+            }
+        }
+        element.addEventListener('click', function (event) {
+            // ask the browser to lock the pointer
+            element.requestPointerLock = element.requestPointerLock ||
+                element.mozRequestPointerLock ||
+                element.webkitRequestPointerLock;
+            element.requestPointerLock();
+        }, false);
+    }
+    
+    /***********************/
+    /*     CROSS-HAIR      */
+    /***********************/
+    crosshair = new THREE.Mesh(
+        new THREE.RingGeometry(0.02, 0.04, 32),
+        new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            opacity: 0.3,
+            transparent: true
+        })
+    );
+    crosshair.position.z = -2;
+    camera.add(crosshair);
 
     /***********************/
     /*      EVENTS         */
@@ -94,25 +145,81 @@ function init()
     renderer.domElement.addEventListener( 'mouseup', onMouseUp, false );
     renderer.domElement.addEventListener( 'touchstart', onMouseDown, false );
     renderer.domElement.addEventListener( 'touchend', onMouseUp, false );
+
+    // hook pointer lock state change events
+    document.addEventListener('pointerlockchange', pointerlockchange, false);
+    document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+    document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+    // document.addEventListener('pointerlockerror', pointerlockerror, false);
+    // document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+    // document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
     document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener('keyup', onKeyUp, false);
 
     // automatically resize renderer
     THREEx.WindowResize(renderer, camera);
     // toggle full-screen on given key press
     THREEx.FullScreen.bindKey( {charCode: 'm'.charCodeAt(0)} );
 
-    // mouse events
+    /***********************/
+    /*  EVENT FUNCTIONS    */
+    /***********************/
     function onMouseDown() { isMouseDown = true; }
     function onMouseUp() { isMouseDown = false; }
     function onKeyDown(e) {
-        // console.log(e.keyCode);
-        // camera.position.y += 1;
+        switch (e.keyCode) {
+            case 38: // up
+            case 87: // W
+                moveForward = true;
+                break;
+            case 37: // left
+            case 65: // A
+                moveLeft = true;
+                break;
+            case 40: // down
+            case 83: // S
+                moveBackward = true;
+                break;
+            case 39: // right
+            case 68: // D
+                moveRight = true;
+                break;
+            case 32: // space
+                if (canJump === true )
+                    velocity.y += 350;
+                canJump = false;
+                break;
+        }
+    }
+
+    function onKeyUp(e) {
+        switch (e.keyCode) {
+            case 38: // up
+            case 87: // W
+                moveForward = false;
+                break;
+            case 37: // left
+            case 65: // A
+                moveLeft = false;
+                break;
+            case 40: // down
+            case 83: // S
+                moveBackward = false;
+                break;
+            case 39: // right
+            case 68: // D
+                moveRight = false;
+                break;
+        }
     }
 
     /***********************/
-    /*      CONTROLS       */
+    /*     RAY CASTER      */
     /***********************/
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    raycaster = new THREE.Raycaster(new THREE.Vector3(), 
+        new THREE.Vector3(0, -1, 0), 0, 10);
 
     /***********************/
     /*      STATS          */
@@ -123,12 +230,6 @@ function init()
     stats.domElement.style.bottom = '0px';
     stats.domElement.style.zIndex = 100;
     container.appendChild(stats.domElement);
-
-    /***********************/
-    /*      LIGHT          */
-    /***********************/
-    var light = new THREE.AmbientLight(0xFFFFFF);
-    scene.add(light);
 
     /***********************/
     /*      GEOMETRY       */
@@ -152,7 +253,7 @@ function init()
 
 function animate() {
     requestAnimationFrame(animate);
-    controls.update();
+    //controls.update();
     stats.update();
     render();
 }
